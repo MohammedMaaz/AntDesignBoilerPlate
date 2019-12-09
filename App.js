6d3b0956d452ee6100a5272c903b752480f310a0
MohammedMaaz <maazproductions25@gmail.com>
2019-12-09 13:17:05 +0500

    ant design boiler plate with firebase and redux

--- App.js
+++ App.js
@@ -1,55 +1,15 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import {Button, WhiteSpace, WingBlank} from '@ant-design/react-native';
 
 export default class App extends Component {
   render() {
     return (
       <WingBlank>
-        <WhiteSpace />
-        <Button onPress={e => console.log('Default')}>default</Button>
-        <WhiteSpace />
-        <Button disabled>default disabled</Button>
-        <WhiteSpace />
-
-        <Button type="primary">primary</Button>
-        <WhiteSpace />
-        <Button type="primary" disabled>
-          primary disabled
-        </Button>
-        <WhiteSpace />
-
-        <Button type="warning">warning</Button>
-        <WhiteSpace />
-        <Button type="warning" disabled>
-          warning disabled
-        </Button>
-        <WhiteSpace />
-
-        <Button loading>loading button</Button>
-
-        <Button activeStyle={false}>无点击反馈</Button>
-        <WhiteSpace />
-        <Button activeStyle={{backgroundColor: 'red'}}>
-          custom feedback style
-        </Button>
-        <WhiteSpace />
-
-        <WingBlank
-          style={{
-            marginTop: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <Button type="ghost">ghost</Button>
-          <Button type="ghost" disabled>
-            ghost disabled
-          </Button>
-          <Button type="ghost" size="small">
-            ghost
-          </Button>
-        </WingBlank>
+        <WhiteSpace size="lg" />
+        <Text>Ant Design Boiler Plate</Text>
+        <WhiteSpace size="lg" />
+        <Button onPress={() => alert('Hi!')}>Welcome</Button>
       </WingBlank>
     );
   }
